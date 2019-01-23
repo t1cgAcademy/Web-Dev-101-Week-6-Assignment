7835d3ec75a622fa8a910180b4d3709ffaca8a6a
Tony DeCarlo <anthony.decarlo@t1cg.com>
2019-01-23 13:13:45 -0500

    assignment answer key

--- README.md
+++ README.md
@@ -1,24 +1,30 @@
 ## Javascript/React Assignment: React Calculator
 
 For this assignment, you will use your knowledge of HTML(JSX), Javascript, and React to
-create a fully functional calculator application.  You have been provided with a
+create a fully functional calculator application. You have been provided with a
 React/Javascript/HTML shell containing some basic code and instructions to get you
 started. Some buttons and Javascript have already been written as examples for you to follow.  
 You must complete the program by adding more buttons with Javascript functionality in
-a React format.  
+a React format.
 
 # Quick Tips
 
-1. Remember the proper way to use/connect event listeners and functions in a React format.  
+1. Remember the proper way to use/connect event listeners and functions in a React format.
 2. Google will be an important resource for individual button functionality creation.
-3. There is some simple math involved.  Try to think of the most logical ways to get a
-button to do what you want.
+3. There is some simple math involved. Try to think of the most logical ways to get a
+   button to do what you want.
 4. It is always important to understand data flow and the overall goal of your application.  
-First, read through the program shell and instructions to get a general idea of what you
-will need to do, and how each piece of data/value, in this case state, is will be used to
-calculate simple math problems.
-5. You have been provided with a CSS file as well.  Since CSS is not the primary focus of
-this assignment, the file has been completely preprogrammed for you.  But remember to make
-sure that corresponding ID and classNames are correct.
+   First, read through the program shell and instructions to get a general idea of what you
+   will need to do, and how each piece of data/value, in this case state, is will be used to
+   calculate simple math problems.
+5. You have been provided with a CSS file as well. Since CSS is not the primary focus of
+   this assignment, the file has been completely preprogrammed for you. But remember to make
+   sure that corresponding ID and classNames are correct.
 6. If you do not remember how to clone and start an application, please refer to past
-assignment instructions regarding this task.
+   assignment instructions regarding this task.
+
+# Assignment
+
+1. Add [square root functionality](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sqrt)
+2. Add [Pi](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/PI) button
+3. Add [e^x](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/E) functionality ( Hint: Use power function from class )
--- src/App/App.jsx
+++ src/App/App.jsx
@@ -13,154 +13,174 @@ class App extends React.Component {
       // set three more states called prevValue, operator, and result to empty strings
     };
   }
-// What State is there?
-// When does it change?
-inputDigit = (digit) => {
-  this.setState({
-    displayValue: this.state.displayValue === '0' ? String(digit) : this.state.displayValue + digit
-  });
-}
-
-inputDot = () => {
-  // The line below checks to see if . is in display value.
-  if (this.state.displayValue.indexOf('.') === -1) {
+  // What State is there?
+  // When does it change?
+  inputDigit = digit => {
     this.setState({
-      displayValue: `${this.state.displayValue }.`
+      displayValue: this.state.displayValue === '0' ? String(digit) : this.state.displayValue + digit
     });
-  }
-  // Set the state of displayValue to equal the state of display value + '.'
-}
+  };
 
-clearDisplay = () => {
-  this.setState({
-    displayValue: '0'
-  });
-}
-// Create clearDisplay function for the clearDisplay event listener below.  Set the state
-// of displayValue to '0'
+  inputDot = () => {
+    // The line below checks to see if . is in display value.
+    if (this.state.displayValue.indexOf('.') === -1) {
+      this.setState({
+        displayValue: `${this.state.displayValue}.`
+      });
+    }
+    // Set the state of displayValue to equal the state of display value + '.'
+  };
 
-toggleSign = () => {
-  const {displayValue} = this.state;
-  const newValue = parseFloat(displayValue) * -1;
-  this.setState({
-    displayValue: String(newValue)
-  });
-}
-
-inputPercent = () => {
-  const value = parseFloat(this.state.displayValue);
-  this.setState({
-    // displayValue: this.state.displayValue / 100
-    displayValue: String(value / 100)
-  });
-}
-
-// Use fat arrow function to define prepareOperation onClick listener created in jsx.
-// Pass operator as an argument for use in the function.
-// set the state of operator to operator, prevValue to this.state.displayValue and
-// set displayValue to '0'
-
-prepareOperation = (operator) => {
-  this.setState({
-    prevValue: this.state.displayValue,
-    displayValue: '0',
-    operator
-  });
-}
-
-equals = (operator, prevValue, displayValue) => {
-  if (this.state.operator === '+') {
-    const finalResult = parseFloat(this.state.displayValue) + parseFloat(this.state.prevValue);
+  clearDisplay = () => {
     this.setState({
-      displayValue: String(finalResult)
+      displayValue: '0'
     });
-  } else if (this.state.operator === '-') {
-    const finalResult = parseFloat(this.state.prevValue) - parseFloat(this.state.displayValue);
-    this.setState({
-      displayValue: String(finalResult)
-    });
-  } else if (this.state.operator === '*') {
-    const finalResult = parseFloat(this.state.displayValue) * parseFloat(this.state.prevValue);
-    this.setState({
-      displayValue: String(finalResult)
-    });
-  } else if (this.state.operator === '/') {
-    const finalResult = parseFloat(this.state.prevValue) / parseFloat(this.state.displayValue);
-    this.setState({
-      displayValue: String(finalResult)
-    });
-  } else if (this.state.operator === 'mod') {
-    const finalResult = parseFloat(this.state.prevValue) % parseFloat(this.state.displayValue);
-    this.setState({
-      displayValue: String(finalResult)
-    });
-  } else if (this.state.operator === '^') {
-    const finalResult = this.power(parseFloat(this.state.prevValue), parseFloat(this.state.displayValue));
-    this.setState({
-      displayValue: String(finalResult)
-    });
-  }
+  };
+  // Create clearDisplay function for the clearDisplay event listener below.  Set the state
+  // of displayValue to '0'
 
-}
+  toggleSign = () => {
+    const {displayValue} = this.state;
+    const newValue = parseFloat(displayValue) * -1;
+    this.setState({
+      displayValue: String(newValue)
+    });
+  };
 
-factorial = (num) => {
-  if (num === 0) {
-    return 1;
-  }
-  return (num * this.factorial(num - 1));
-}
+  inputPercent = () => {
+    const value = parseFloat(this.state.displayValue);
+    this.setState({
+      // displayValue: this.state.displayValue / 100
+      displayValue: String(value / 100)
+    });
+  };
 
-power = (num, exp) => {
-  if (exp === 0) {
-    return 1;
-  }
-  return num * this.power(num, exp - 1);
-}
+  // Use fat arrow function to define prepareOperation onClick listener created in jsx.
+  // Pass operator as an argument for use in the function.
+  // set the state of operator to operator, prevValue to this.state.displayValue and
+  // set displayValue to '0'
 
-inputFactorial = () => {
-  const value = parseFloat(this.state.displayValue);
-  this.setState({
-    displayValue: String(this.factorial(value))
-  });
-}
+  prepareOperation = operator => {
+    this.setState({
+      prevValue: this.state.displayValue,
+      displayValue: '0',
+      operator
+    });
+  };
 
-render() {
-  return (
-    <div>
-      <h3 id={style.numberDisplay}>{this.state.displayValue}</h3>
-      <div className={style.operations}>
-        <button onClick={() => this.clearDisplay()}>AC</button>
-        <button onClick={() => this.inputPercent()}>%</button>
-        <button onClick={() => this.toggleSign()}>±</button>
-        <button onClick={() => this.inputFactorial()}>!</button>
+  equals = (operator, prevValue, displayValue) => {
+    if (this.state.operator === '+') {
+      const finalResult = parseFloat(this.state.displayValue) + parseFloat(this.state.prevValue);
+      this.setState({
+        displayValue: String(finalResult)
+      });
+    } else if (this.state.operator === '-') {
+      const finalResult = parseFloat(this.state.prevValue) - parseFloat(this.state.displayValue);
+      this.setState({
+        displayValue: String(finalResult)
+      });
+    } else if (this.state.operator === '*') {
+      const finalResult = parseFloat(this.state.displayValue) * parseFloat(this.state.prevValue);
+      this.setState({
+        displayValue: String(finalResult)
+      });
+    } else if (this.state.operator === '/') {
+      const finalResult = parseFloat(this.state.prevValue) / parseFloat(this.state.displayValue);
+      this.setState({
+        displayValue: String(finalResult)
+      });
+    } else if (this.state.operator === 'mod') {
+      const finalResult = parseFloat(this.state.prevValue) % parseFloat(this.state.displayValue);
+      this.setState({
+        displayValue: String(finalResult)
+      });
+    } else if (this.state.operator === '^') {
+      const finalResult = this.power(parseFloat(this.state.prevValue), parseFloat(this.state.displayValue));
+      this.setState({
+        displayValue: String(finalResult)
+      });
+    }
+  };
+
+  factorial = num => {
+    if (num === 0) {
+      return 1;
+    }
+    return num * this.factorial(num - 1);
+  };
+
+  power = (num, exp) => {
+    if (exp === 0) {
+      return 1;
+    }
+    return num * this.power(num, exp - 1);
+  };
+
+  inputFactorial = () => {
+    const value = parseFloat(this.state.displayValue);
+    this.setState({
+      displayValue: String(this.factorial(value))
+    });
+  };
+
+  inputSqrt = () => {
+    const value = parseFloat(this.state.displayValue);
+    this.setState({
+      displayValue: String(Math.sqrt(value))
+    });
+  };
+
+  inputPi = () => {
+    this.setState({
+      displayValue: Math.PI
+    });
+  };
+
+  inputEPowerX = () => {
+    const value = parseFloat(this.state.displayValue);
+    this.setState({
+      displayValue: String(this.power(Math.E, value))
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3 id={style.numberDisplay}>{this.state.displayValue}</h3>
+        <div className={style.operations}>
+          <button onClick={() => this.clearDisplay()}>AC</button>
+          <button onClick={() => this.inputPercent()}>%</button>
+          <button onClick={() => this.toggleSign()}>±</button>
+          <button onClick={() => this.inputFactorial()}>!</button>
+        </div>
+        <div className={style.numberFields}>
+          <button onClick={() => this.inputDigit(0)}>0</button>
+          <button onClick={() => this.inputDot()}>•</button>
+          <button onClick={() => this.inputDigit(1)}>1</button>
+          <button onClick={() => this.inputDigit(2)}>2</button>
+          <button onClick={() => this.inputDigit(3)}>3</button>
+          <button onClick={() => this.inputDigit(4)}>4</button>
+          <button onClick={() => this.inputDigit(5)}>5</button>
+          <button onClick={() => this.inputDigit(6)}>6</button>
+          <button onClick={() => this.inputDigit(7)}>7</button>
+          <button onClick={() => this.inputDigit(8)}>8</button>
+          <button onClick={() => this.inputDigit(9)}>9</button>
+          <button onClick={() => this.inputPi()}>Pi</button>
+          <button onClick={() => this.inputSqrt()}>sqrt</button>
+          <button onClick={() => this.inputEPowerX()}>e^x</button>
+        </div>
+        <div className={style.operations}>
+          <button onClick={() => this.prepareOperation('/')}>÷</button>
+          <button onClick={() => this.prepareOperation('*')}>x</button>
+          <button onClick={() => this.prepareOperation('-')}>-</button>
+          <button onClick={() => this.prepareOperation('+')}>+</button>
+          <button onClick={() => this.prepareOperation('mod')}>mod</button>
+          <button onClick={() => this.prepareOperation('^')}>^</button>
+          <button onClick={() => this.equals()}>=</button>
+        </div>
       </div>
-      <div className={style.numberFields}>
-        <button onClick={() => this.inputDigit(0)}>0</button>
-        <button onClick={() => this.inputDot()}>•</button>
-        <button onClick={() => this.inputDigit(1)}>1</button>
-        <button onClick={() => this.inputDigit(2)}>2</button>
-        <button onClick={() => this.inputDigit(3)}>3</button>
-        <button onClick={() => this.inputDigit(4)}>4</button>
-        <button onClick={() => this.inputDigit(5)}>5</button>
-        <button onClick={() => this.inputDigit(6)}>6</button>
-        <button onClick={() => this.inputDigit(7)}>7</button>
-        <button onClick={() => this.inputDigit(8)}>8</button>
-        <button onClick={() => this.inputDigit(9)}>9</button>
-        <button onClick={() => this.inputDigit('(')}>(</button>
-        <button onClick={() => this.inputDigit(')')}>)</button>
-      </div>
-      <div className={style.operations}>
-        <button onClick={() => this.prepareOperation('/')}>÷</button>
-        <button onClick={() => this.prepareOperation('*')}>x</button>
-        <button onClick={() => this.prepareOperation('-')}>-</button>
-        <button onClick={() => this.prepareOperation('+')}>+</button>
-        <button onClick={() => this.prepareOperation('mod')}>mod</button>
-        <button onClick={() => this.prepareOperation('^')}>^</button>
-        <button onClick={() => this.equals()}>=</button>
-      </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default App;
